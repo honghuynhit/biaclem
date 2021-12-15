34880b72f121e4ed940f7dfd268ce62d77674144
Huỳnh Gà <honghuynhit@gmail.com>
2021-12-15 09:56:44 +0700

    update

--- src/types/navigation.tsx
+++ src/types/navigation.tsx
@@ -1,11 +1,7 @@
 export type MainStackParamList = {
 	Home: undefined;
-	Buttons: undefined;
-	Colors: undefined;
 	BauWaiKhonPadum: undefined;
 	BauHnoiDaSuqDo: undefined;
 	CleuHaroih: any;
-	Nice: undefined;
 	ListCaleuHadroih: any;
-	Details: any,
 };
